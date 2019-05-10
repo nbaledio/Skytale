d1e5e031ce7bb680ac449858a87b62cff5378a78
Sophia Santos <solsanto@ucsc.edu>
2019-05-09 23:24:57 -0700

    Added menu

--- Untitled_Game/js/main.js
+++ Untitled_Game/js/main.js
@@ -7,13 +7,15 @@
 //Variables
 var width = 800;
 var height = 450;
+var style = { font: '24px Comic Sans MS', fill: '#FFF', align: "center" };
 
 //Game
 var game = new Phaser.Game(width, height, Phaser.AUTO, 'phaser');
 
 // add states to StateManager
+game.state.add('menu', Menu);
 game.state.add('test_environment', test_environment);
 game.state.add('test_environment2', test_environment2);
 game.state.add('level_1', level_1);
 
-game.state.start('level_1');
+game.state.start('menu');
